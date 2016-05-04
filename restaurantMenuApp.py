572664a312d16238c05330f11133fa84965e163d
dbw1 <dbw401@gmail.com>
2016-05-04 13:13:16 -0700

    Created html templates to support each web app functional view --> FakeMenuItems.py - creates a fake db for testing restaurantMenuApp.py - altered to add rendering and linking to proper templates templates/ - templates created to represent each of the pages Templates tested to see functionality

--- restaurantMenuApp.py
+++ restaurantMenuApp.py
@@ -1,48 +1,56 @@
-from flask import Flask
+from flask import Flask, render_template
+import FakeMenuItems 
 app = Flask(__name__)
 
+#test restaurants and menus because we don't have an actual db yet
+
+restaurants = FakeMenuItems.restaurants
+restaurant = FakeMenuItems.restaurant
+
+items = FakeMenuItems.items
+item = FakeMenuItems.item
 
 @app.route('/')
 @app.route('/restaurants/')
 def restaurantsHome():
-	output="You are at Restaurant Home"
-	return (output)
+	# new home logic
+	return render_template('restaurantHome.html', restaurant_list=restaurants)
 
 @app.route('/restaurant/new/')
 def newRestaurant():
-	output="Add new restaurant here"
-	return (output)
+	
+	return render_template('newRestaurant.html', restaurant=restaurants)
 
 @app.route('/restaurant/<int:restaurant_id>/edit/')
 def editRestaurant(restaurant_id):
-	output="Edit restaurant here"
-	return (output)
+	
+	return render_template('editRestaurant.html', restaurant=restaurant)
 
 @app.route('/restaurant/<int:restaurant_id>/delete/')
 def deleteRestaurant(restaurant_id):
-	output="Delete restaurant here"
-	return (output)
+	
+	return render_template('deleteRestaurant.html', restaurant=restaurant)
 
 @app.route('/restaurant/<int:restaurant_id>/menu/')
 @app.route('/restaurant/<int:restaurant_id>/')
 def menuHome(restaurant_id):
-	output="Menu Home of a particular restaurant here"
-	return (output)
+	
+	return render_template('menuHome.html', restaurant=restaurant, items=items)
 
 @app.route('/restaurant/<int:restaurant_id>/menu/new/')
 def newMenuItem(restaurant_id):
-	output="new Menu item here"
-	return (output)
+	
+	return render_template('newMenuItem.html', restaurant=restaurant)
 
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit/')
 def editMenuItem(restaurant_id, menu_id):
-	output="Edit item details here"
-	return (output)
+	
+	return render_template('editMenuItem.html', restaurant=restaurant, item=item, menuID=menu_id)
 
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete/')
 def deleteMenuItem(restaurant_id, menu_id):
-	output="Delete item details here"
-	return (output)
+	
+	return render_template('deleteMenuItem.html', restaurant=restaurant, item=item, menuID=menu_id)
 	
 
 
